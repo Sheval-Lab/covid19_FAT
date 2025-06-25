619d8bb433758a32e1a89a465cf72d827bd47ee8
Anna Valyaeva <kish.mishintheuniverse@gmail.com>
2025-06-25 19:50:38 +0000

    update dataset filtering; add KEGG

--- 02_scRNAseq-ORA.R
+++ 02_scRNAseq-ORA.R
@@ -1,6 +1,7 @@
 # Load libraries ---------------------------------------------------------------
 library(tidyverse)
 library(clusterProfiler)
+library(KEGG.db)
 source("revigo.R")
 
 
@@ -10,6 +11,15 @@ res_dir <- file.path("results", "scRNAseq", "02_ORA")
 
 
 # Input data -------------------------------------------------------------------
+## Datasets meta
+ds_meta <- read_tsv("sc_dataset_meta.txt")
+
+### Exclude datasets of organoids/cells infected with SARS-CoV-2 in vitro
+datasets_2exclude <- c(
+  "GSE159556", "GSE182298", "GSE167747", "GSE178404", "GSE208034", "GSE166766",
+  "GSE156760", "GSE151878")
+
+
 ## DGEA results 
 degs_files <- list.files(data_dir, pattern = "*.degs.txt", full.names = TRUE)
 
@@ -26,7 +36,18 @@ degs <- map(degs_files, read_tsv, id = "dataset") %>%
 # Filter DGEA results ----------------------------------------------------------
 degs_flt <- degs %>% 
   filter(status != "Stable") %>% 
-  dplyr::select(gene, status, celltype, dataset)
+  dplyr::select(gene, status, celltype, dataset) %>% 
+  # Rename mitochondrial genes to be found in org.Hs.eg.db
+  mutate(gene = str_remove(gene, "^MT-")) 
+  
+
+## Convert SYMBOL to ENTREZ IDs ------------------------------------------------
+degs_entrez <- bitr(degs_flt$gene, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Hs.eg.db") %>% 
+  filter(!duplicated(ENTREZID), !duplicated(SYMBOL)) 
+
+degs_flt <- degs_flt %>% 
+  left_join(degs_entrez, by = c("gene" ="SYMBOL"))
+
 
 ## Save combined table with DEGs from all datasets
 write_tsv(degs_flt, file.path(res_dir, "degs_combined_table.txt"))
@@ -35,8 +56,6 @@ write_tsv(degs_flt, file.path(res_dir, "degs_combined_table.txt"))
 # Group DEGs by cell type (referred to as subtype in SCovid DB) and dataset ----
 ## Make nested column for dataset:tissue:UpDown groupping 
 degs_by_celltype <- degs_flt %>% 
-  # Rename mitochondrial genes to be found in org.Hs.eg.db
-  mutate(gene = str_remove(gene, "^MT-")) %>% 
   group_by(celltype, dataset, status) %>% 
   nest() %>% 
   ungroup() %>% 
@@ -46,34 +65,48 @@ degs_by_celltype <- degs_flt %>%
 # Functional enrichment --------------------------------------------------------
 ## Function for GO BP Over-Representation Analysis (ORA)
 run_enrichGO <- function(data){
-  data$go_bp = vector(mode = "list", length = nrow(data))
-  for (i in 1:nrow(data)){
-    data$go_bp[[i]] = as.data.frame(enrichGO(gene = data$data[[i]]$gene, OrgDb = "org.Hs.eg.db", keyType = "SYMBOL", ont = "BP"))
-  }
-  return(data)
+  go_bp = as.data.frame(enrichGO(gene = data$gene, OrgDb = "org.Hs.eg.db", keyType = "SYMBOL", ont = "BP"))
+  return(go_bp)
 }
 
-## Run ORA
-go_by_celltype <- run_enrichGO(degs_by_celltype)
+
+## Function for KEGG Over-Representation Analysis (ORA)
+run_enrichKEGG <- function(data){
+  gene_list = data %>% drop_na(ENTREZID) %>% pull(ENTREZID)
+  kegg = enrichKEGG(gene = gene_list, organism = "hsa", use_internal_data = TRUE)
+  # Convert EntrezIDs to gene symbols
+  if (!is.null(kegg)) {
+    kegg = setReadable(kegg, OrgDb = "org.Hs.eg.db", keyType = "ENTREZID")
+  }
+  kegg = as.data.frame(kegg)
+  return(kegg)
+}
 
 
-## Save RDS object with ORA results 
-saveRDS(go_by_celltype, file.path(res_dir, "go_by_celltype.rds"))
+## Run ORA ---------------------------------------------------------------------
+ora_by_celltype$go_bp <- map(degs_by_celltype$data, run_enrichGO)
+ora_by_celltype$kegg <- map(ora_by_celltype$data, run_enrichKEGG)
+
+
+## Save RDS object with ORA results --------------------------------------------
+saveRDS(ora_by_celltype, file.path(res_dir, "ora_by_celltype.rds"))
 
 
 # Filter GO BP terms -----------------------------------------------------------
 ## Parse ORA results - add dataset, cell type, and Up/Down status info
-go_by_celltype_df <- go_by_celltype$go_bp %>% 
-  map2(go_by_celltype$celltype_dataset_status, ., ~ add_column(.y, celltype_dataset_status = .x)) %>% 
+go_by_celltype_df <- ora_by_celltype$go_bp %>% 
+  map2(ora_by_celltype$celltype_dataset_status, ., ~ add_column(.y, celltype_dataset_status = .x)) %>% 
   list_rbind() %>% 
   separate(celltype_dataset_status, into = c("celltype", "dataset", "status"), sep = "//")
 
 
 ## Lipid-related words
 lipid_terms <- c("lipid", "fat", "triglyceride", "cholesterol")
-nonlipid_terms <- c("fate", "sulfat")
+nonlipid_terms <- c("fate", "sulfat", "liver disease")
 
 go_lipid <- go_by_celltype_df %>%
+  # Remove 'in vitro' datasets 
+  dplyr::filter(str_detect(dataset, paste(datasets_2exclude, collapse = "|"), negate = TRUE)) %>% 
   # Keep only significant GO BP terms
   dplyr::filter(qvalue <= 0.05) %>% 
   # Keep only GO BP terms related to lipids metabolism
@@ -117,4 +150,45 @@ go_lipid_revigo_imp_res <- go_lipid %>%
 
 ## Save filtered ORA results 
 write_tsv(go_lipid_revigo_imp_res, file.path(res_dir, "go_lipid_revigo_imp_res.txt"))
+
+
+# Filter KEGG pathways ---------------------------------------------------------
+## Parse ORA results - add dataset, cell type, and Up/Down status info
+kegg_by_celltype_df <- ora_by_celltype$kegg %>% 
+  map2(ora_by_celltype$celltype_dataset_status, ., ~ add_column(.y, celltype_dataset_status = .x)) %>% 
+  list_rbind() %>% 
+  separate(celltype_dataset_status, into = c("celltype", "dataset", "status"), sep = "//")
+
+
+kegg_lipid <- kegg_by_celltype_df %>%
+  # Remove 'in vitro' datasets 
+  dplyr::filter(str_detect(dataset, paste(datasets_2exclude, collapse = "|"), negate = TRUE)) %>% 
+  # Keep only significant KEGG pathways
+  dplyr::filter(qvalue <= 0.05) %>% 
+  # Keep only KEGG pathways related to lipids metabolism
+  dplyr::filter(str_detect(str_to_lower(Description), paste(lipid_terms, collapse = "|"))) %>%
+  # Remove some KEGG pathways
+  dplyr::filter(!str_detect(str_to_lower(Description), paste(nonlipid_terms, collapse = "|")))
+
+
+## Save KEGG ORA results
+write_tsv(kegg_lipid, file.path(res_dir, "kegg_lipid.txt"))
+
+
+# Retrieve DE genes in GO terms and KEGG pathways ------------------------------
+## GO BP
+go_lipid_revigo_imp_2genetable <- go_lipid_revigo_imp_res %>% 
+  left_join(ds_meta, by = "dataset") %>% 
+  dplyr::select(ID, Description, geneID, status, celltype, Tissue, dataset, `Dataset name`) 
+
+write_tsv(go_lipid_revigo_imp_2genetable, file.path(res_dir, "go_lipid_revigo_imp_degs.txt"))
+
+
+## KEGG
+kegg_lipid_2genetable <- kegg_lipid %>% 
+  left_join(ds_meta, by = "dataset") %>% 
+  dplyr::select(ID, Description, geneID, status, celltype, Tissue, dataset, `Dataset name`) 
+
+write_tsv(kegg_lipid_2genetable, file.path(res_dir, "kegg_lipid_degs.txt"))
+
 
